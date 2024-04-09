c8b9adfab53def3c6757e154f29e92995110de45
Thomas V <2619415+tvillaren@users.noreply.github.com>
2024-04-09 23:21:32 +0200

    fix: replacing "." by its SINGLE_COMMAND_CLI_SYMBOL to check if oclif/test works

--- src/util/ids.ts
+++ src/util/ids.ts
@@ -1,6 +1,11 @@
 import {Config} from '../interfaces'
+import {SINGLE_COMMAND_CLI_SYMBOL} from '../symbols'
 
 export function toStandardizedId(commandID: string, config: Config): string {
+  if (commandID === '.') {
+    return SINGLE_COMMAND_CLI_SYMBOL
+  }
+
   return commandID.replaceAll(new RegExp(config.topicSeparator, 'g'), ':')
 }
 
